98bab7a67999596f28e014d9ea660b54681f9c1e
davidlandau770 <dal778770@gmail.com>
2026-05-11 16:57:28 +0300

    fix

--- server/src/helpers/cloudinary.ts
+++ server/src/helpers/cloudinary.ts
@@ -1,12 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 import { Readable } from "stream";
-import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } from "./environments";
-
-cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
-});
 
 export const uploadToCloudinary = (buffer: Buffer): Promise<string> =>
   new Promise((resolve, reject) => {
